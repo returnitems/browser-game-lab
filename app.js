b74adbb31fd37b5065e5402ab5f4c21a8355a719
diwanumang <diwan.umang@gmail.com>
2024-04-05 14:05:06 -0400

    No changes

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const mainBox = document.querySelector(".mainBox");
 
 const gameBox = document.querySelector(".gameBox");
 
-// const startButton = document.createElement("button");
-// startButton.setAttribute("type", "button");
-// startButton.setAttribute("class", "startButton");
-// startButton.textContent = "Start Game!";
-// gameBox.appendChild(startButton);
+const startButton = document.createElement("button");
+startButton.setAttribute("type", "button");
+startButton.setAttribute("class", "startButton");
+startButton.textContent = "Start Game!";
+gameBox.appendChild(startButton);
+
 
 const username = () => {
     startButton.remove();
