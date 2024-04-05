6fb1491a1e22aa3da8d51292e3f9d1c051485e1c
diwanumang <diwan.umang@gmail.com>
2024-04-05 11:49:39 -0400

    created a function which would place input when start button clicked

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-const mainBox = document.querySelector('.mainBox');
+const mainBox = document.querySelector(".mainBox");
 
-const gameBox = document.querySelector('.gameBox');
+const gameBox = document.querySelector(".gameBox");
 
+const startButton = document.createElement("button");
+startButton.setAttribute("type", "button");
+startButton.setAttribute("class", "startButton");
+startButton.textContent = "Start Game!";
+gameBox.appendChild(startButton);
 
+const username = () => {
+  const inputBox = document.createElement("input");
+  gameBox.appendChild(inputBox);
+};
 
-
-
-const inputBox = document.createElement('input');
-gameBox.appendChild(inputBox);
+startButton.addEventListener("click", username);
